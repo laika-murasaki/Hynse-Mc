e3f2da08c7628b9fff094a01de06f2efd9be78e3
Laika <111684641+laika-murasaki@users.noreply.github.com>
2022-12-28 01:08:03 +0700

    okieaaaaaaa

--- Instance/Survival/plugins/PlaceholderAPI/javascripts/discord_voice_stats.js
+++ Instance/Survival/plugins/PlaceholderAPI/javascripts/discord_voice_stats.js
@@ -2,7 +2,7 @@ var placeholder = "%voicechat_prefix_uses_voicechat%";
 var voice = PlaceholderAPI.static.setPlaceholders(BukkitPlayer, placeholder);
 
 var checkVoice = function (voice) {
-  return voice === "§a" ? "&#aaff00|" : "&#525e3a|";
+  return voice === "§a" ? ":mic_on:" : ":mic_off:";
 };
 
 checkVoice(voice);
