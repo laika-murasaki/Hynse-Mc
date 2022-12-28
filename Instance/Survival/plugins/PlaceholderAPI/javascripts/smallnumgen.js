a45433131a96f1a3c7b36dcfffdbf0e4fb4dd773
Laika <111684641+laika-murasaki@users.noreply.github.com>
2022-12-28 17:29:32 +0700

    better f3 format

---
+++ Instance/Survival/plugins/PlaceholderAPI/javascripts/smallnumgen.js
@@ -0,0 +1,6 @@
+function smallnum() { 
+    var num = args[0].toLowerCase();
+    num = num.replaceAll("1", "₁").replaceAll("2", "₂").replaceAll("3", "₃").replaceAll("4", "₄").replaceAll("5", "₅").replaceAll("6", "₆").replaceAll("7", "₇").replaceAll("8", "₈").replaceAll("9", "₉").replaceAll("h", "ʜ").replaceAll("m", "ᴍ").replaceAll("s", "ꜱ");
+    return num;
+}
+smallnum();
